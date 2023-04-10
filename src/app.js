a363f2bddccf11ed88e6a362429c902ed132ee78
Breno <brennoohudson@gmail.com>
2023-04-09 23:36:57 -0300

    tweets

--- src/app.js
+++ src/app.js
@@ -2,36 +2,40 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-var usuario =[
-    {
-        name: "bobesponja",
-        avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
-    }
-]
+var usuario =[]
 
 
-var lista = [
-    {
-        username: "bobesponja",
-		avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-		tweet: "Eu amo hambúrguer de siri!"
-    }
-]
-
-app.get("/", (req, res) => {
-    res.send("Sejá bem vindo!")
-});
+var lista = []
 
 app.get("/tweets", (req, res)=>{
     res.send(lista)
 })
 
+app.post("/tweets", (req, res)=>{
+    console.log(req.body.username)
+    if(!req.body.username)
+    {        
+        res.send("UNAUTHORIZED")
+    } else{
+        lista.push({username : req.body.username, tweet: req.body.tweet})
+        res.send(lista)
+    }
+
+})
+
 app.post('/sign-up', (req, res)=> {
-    let name = req.body.name;
-    usuario[(usuario.length)] = name;
-    return res.json([usuario[(usuario.length-1)]])
+    const { username, avatar } = req.body
+
+    if(!username||!avatar){
+        res.status(400).send("Todos os campos são obrigatorios")
+        return
+    }
+
+    usuario.push({id: usuario.length+1, username,avatar});
+    res.status(201).send("OK")
 })
 
 const PORT = 5000;
